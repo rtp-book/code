a8864462303d5d66bdaa159b5a0a8fb1aefb1455
John Sheehan <jennasyseng@gmail.com>
2020-10-31 09:42:41 -0700

    Added ch08 code

---
+++ ch08/pyreact2.py
@@ -0,0 +1,21 @@
+# __pragma__ ('skip')
+# These are here to quiet the Python linter and are ignored by transcrypt
+React = None
+ReactDOM = None
+document = None
+# __pragma__ ('noskip')
+
+# Map React javaScript objects to Python identifiers
+createElement = React.createElement
+useState = React.useState
+
+def render(root_component, props, container):
+    """Loads main react component into DOM"""
+    def main():
+        ReactDOM.render(
+            React.createElement(root_component, props),
+            document.getElementById(container)
+        )
+
+    document.addEventListener("DOMContentLoaded", main)
+
