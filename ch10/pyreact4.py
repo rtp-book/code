abb55c38eeb2d600524fbcf7ce85e30022643302
John Sheehan <jennasyseng@gmail.com>
2020-11-01 01:55:58 -0800

    Added ch10 code

---
+++ ch10/pyreact4.py
@@ -0,0 +1,20 @@
+# Load React and ReactDOM JavaScript libraries into local namespace
+React = require('react')
+ReactDOM = require('react-dom')
+
+# Map React javaScript objects to Python identifiers
+createElement = React.createElement
+useState = React.useState
+
+
+def render(root_component, props, container):
+    """Loads main react component into DOM"""
+
+    def main():
+        ReactDOM.render(
+            React.createElement(root_component, props),
+            document.getElementById(container)
+        )
+
+    document.addEventListener("DOMContentLoaded", main)
+
