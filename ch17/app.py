d6de6e1653c146ac43ef135ab1a9f705e80dc521
John Sheehan <jennasyseng@gmail.com>
2020-11-05 14:15:04 -0800

    Swapped ch16 and ch17

--- ch17/app.py
+++ ch17/app.py
@@ -1,41 +1,136 @@
-from pyreact import useState, render, createElement
-from listItems import ListItems
+from pyreact import setTitle, useEffect, useState, render, createElement as el
 
 def App():
-    newItem, setNewItem = useState("")
-    editItem, setEditItem = useState("")
-    listItems, setListItems = useState([])
+    newTask, setNewTask = useState("")
+    editTask, setEditTask = useState(None)
+    taskList, setTaskList = useState([])
+    taskCount, setTaskCount = useState(0)
+    taskFilter, setTaskFilter = useState("all")
 
     def handleSubmit(event):
         event.preventDefault()
-        new_list = list(listItems)  # Make a copy
-        if len(editItem) > 0:  # In edit mode
-            new_list[new_list.index(editItem)] = newItem
+        new_list = list(taskList)  # Make a copy
+        if editTask is not None:  # In edit mode
+            taskIndex = new_list.index(editTask)  # Get list position
+            new_list[taskIndex].update({'name': newTask})  # Update name
         else:  # In add mode
-            new_list.append(newItem)  # Add the new item
-        setListItems(new_list)  # Update our state
-        setNewItem("")  # Clear the new item value
-        setEditItem("")  # Clear the edit item value
+            new_list.append({'name': newTask, 'status': False})  # Add new item
+        setTaskList(new_list)  # Update our state
+        setNewTask("")  # Clear the new item value
+        setEditTask(None)  # Clear the edit item value
+
+    def handleEdit(task):
+        setNewTask(task['name'])  # Set the new item value
+        setEditTask(task)  # Set the edit item value
+
+    def handleDelete(task):
+        new_list = list(taskList)  # Make a copy
+        new_list.remove(task)  # Remove the specified item
+        setTaskList(new_list)  # Update our state
 
     def handleChange(event):
         target = event['target']
-        setNewItem(target.value)
+        if target['name'] == 'taskFilter':
+            setTaskFilter(target['value'])
+        else:
+            setNewTask(target['value'])
 
-    def handleDelete(item):
-        new_list = list(listItems)  # Make a copy
-        new_list.remove(item)  # Remove the specified item
-        setListItems(new_list)  # Update our state
+    def handleChangeStatus(event, task):
+        target = event['target']
+        new_list = list(taskList)  # Make a copy
+        taskIndex = new_list.index(task)  # Get list position
+        new_list[taskIndex].update({'status': target['checked']})  # Update
+        setTaskList(new_list)  # Update our state
 
-    def handleEdit(item):
-        setNewItem(item)  # Set the new item value
-        setEditItem(item)  # Set the edit item value
-
-    return __pragma__('xtrans',
-                      'jsx-transform --factory createElement',
-                      '{}',
-                      __include__('app.jsx')
+    def ListItem(props):
+        task = props['task']
+        if taskFilter == "all" or \
+                (taskFilter == "open" and not task['status']) or \
+                (taskFilter == "closed" and task['status']):
+            return el('li', None,
+                      task['name'] + " ",
+                      el('button',
+                         {'type': 'button',
+                          'onClick': lambda: handleDelete(task)
+                         }, "Delete"
+                        ),
+                      el('button',
+                         {'type': 'button',
+                          'onClick': lambda: handleEdit(task)
+                         }, "Edit"
+                        ),
+                      el('label', {'htmlFor': 'status'}, " Completed:"),
+                      el('input',
+                         {'type': 'checkbox',
+                          'id': 'status',
+                          'onChange': lambda e: handleChangeStatus(e, task),
+                          'checked': task['status']
+                         }
+                        ),
                      )
+        else:
+            return None
 
+    def ListItems():
+        return [el(ListItem, {'key': task['name'], 'task': task}) for task in taskList]
+
+    def updateCount():
+        if taskFilter == 'open':
+            new_list = [task for task in taskList if not task['status']]
+        elif taskFilter == 'closed':
+            new_list = [task for task in taskList if task['status']]
+        else:
+            new_list = [task for task in taskList]
+
+        setTaskCount(len(new_list))
+
+    useEffect(lambda: setTitle("ToDo List"), [])
+    useEffect(updateCount, [taskList, taskFilter])
+
+    return el('form', {'onSubmit': handleSubmit},
+              el('div', None, "Number of Tasks: {}".format(taskCount)),
+              el('div', None,
+                 el('label', {'htmlFor': 'all'}, "All Tasks:"),
+                 el('input', {'type': 'radio',
+                              'name': 'taskFilter',
+                              'id': 'all',
+                              'value': 'all',
+                              'onChange': handleChange,
+                              'checked': taskFilter == 'all'
+                             }
+                   ),
+                 el('label', {'htmlFor': 'open'}, " Active:"),
+                 el('input', {'type': 'radio',
+                              'name': 'taskFilter',
+                              'id': 'open',
+                              'value': 'open',
+                              'onChange': handleChange,
+                              'checked': taskFilter == 'open'
+                             }
+                   ),
+                 el('label', {'htmlFor': 'closed'}, " Completed:"),
+                 el('input', {'type': 'radio',
+                              'name': 'taskFilter',
+                              'id': 'closed',
+                              'value': 'closed',
+                              'onChange': handleChange,
+                              'checked': taskFilter == 'closed'
+                             }
+                   ),
+                ),
+              el('label', {'htmlFor': 'newItem'},
+                 "Edit Item: " if editTask is not None else "Add Item: "
+                ),
+              el('input', {'id': 'newItem',
+                           'onChange': handleChange,
+                           'value': newTask
+                          }
+                ),
+              el('input', {'type': 'submit'}),
+              el('ol', None,
+                 el(ListItems, None)
+                ),
+             )
 
 render(App, None, 'root')
 
