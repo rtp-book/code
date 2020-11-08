f54620795ac706deaed242b2f6aabc8d84a721d8
John Sheehan <jennasyseng@gmail.com>
2020-11-08 11:55:53 -0800

    Updated ch23 files

---
+++ ch23/jsutils.py
@@ -0,0 +1,16 @@
+console = window.console
+
+deepcopy = require('deepcopy')
+
+polyfill = require("@babel/polyfill")  # required by async/await
+
+async def fetch(url, callback):
+    try:
+        response = await window.fetch(url)
+        if response.status != 200:
+            console.error('Fetch error - Status Code: ' + response.status)
+        else:
+            data = await response.json()
+            callback(data)
+    except object as e:
+        console.error(e)
