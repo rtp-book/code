c558765de2318f81f0b813d93a69be2dba26765a
John Sheehan <jennasyseng@gmail.com>
2020-12-09 00:53:30 -0800

    Removed transcrypt bundler config & changed proxy server

--- ch21/dev-server.js
+++ ch21/dev-server.js
@@ -1,58 +1,25 @@
-const { createServer } = require('http');
-const { createProxyServer } = require('http-proxy');
-const Path = require('path');
 const Bundler = require('parcel-bundler');
+const express = require('express');
+const { createProxyMiddleware } = require('http-proxy-middleware');
 
-const flaskServer = {
-  protocol: 'http',
-  host: 'localhost',
-  port: 8000
-};
 
-const parcelServer = {
-  protocol: 'http',
-  host: 'localhost',
-  port: 1234
-};
+const app = express();
+
+const apiProxy = createProxyMiddleware('/api', {
+  target: 'http://localhost:8000',
+  pathRewrite: {'^/api': ''}
+});
+app.use(apiProxy);
 
 // parcel options
 const options = {minify:false, cache: false, outDir: 'dist/dev', logLevel: 4};
 
-// point parcel at its entry point
-const entryFiles = Path.join(__dirname, '.', 'index.html');
+const bundler = new Bundler('./index.html', options);
+app.use(bundler.middleware());
 
-// init the bundler
-const bundler = new Bundler(entryFiles, options);
-bundler.serve();
-
-// create a proxy server instance
-const proxy = createProxyServer();
-
-// web server
-const server = createServer((req, res) => {
-  if (req.url.startsWith('/api/')) {
-    //strip off api prefix if going to app server
-    req.url = req.url.slice('/api'.length);
-    proxy.web(req, res, {
-      // back-end server
-      target: flaskServer,
-      changeOrigin: true,
-      autoRewrite: true
-    }, function(e) {
-      console.error("ERROR: ", e.message);
-      res.writeHead(500, { 'Content-Type': 'text/plain' });
-      res.write('Internal Server Error');
-      res.end();
-    });
-  } else {
-    // parcel's dev server
-    proxy.web(req, res, {
-      target: parcelServer,
-      ws: true
-    });
-  }
+bundler.on('buildEnd', () => {
+  console.log('Parcel proxy server has started at: http://localhost:8080');
 });
 
-console.log('dev proxy server operating at: http://localhost:8080/');
-server.listen(8080);
+app.listen(8080);
 
